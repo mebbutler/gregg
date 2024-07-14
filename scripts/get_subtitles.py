996ba2d9ab4fd1a5a9a5f421db8d80976f69d809
Madeleine Butler <maddyebutler@gmail.com>
2024-07-14 11:02:02 +0100

    added logs

--- scripts/get_subtitles.py
+++ scripts/get_subtitles.py
@@ -3,6 +3,14 @@
 import requests
 import argparse
 from pathlib import Path
+import logging
+
+logger = logging.getLogger('logs')
+logger.setLevel(logging.DEBUG)
+
+ch_logs = logging.StreamHandler()
+ch_logs.setLevel(logging.INFO)
+logger.addHandler(ch_logs)
 
 parser = argparse.ArgumentParser(
                     prog='get_subtitles',
@@ -16,6 +24,8 @@ args = parser.parse_args()
 # set the url
 series_url = f"https://subsaga.com/bbc/entertainment/masterchef/series-{args.series}/"
 
+logger.info(f"Fetching subtitles for series {args.series} from {series_url}")
+
 # set up directories to save subtitle data
 # skips if the directories already exist
 subtitles_dir = Path(__file__).parent.parent / f"data/subtitles"
@@ -23,9 +33,12 @@ subtitles_dir.mkdir(exist_ok=True)
 series_dir = subtitles_dir / f"series_{args.series}"
 series_dir.mkdir(exist_ok=True)
 
+logger.info(f"Made subtitle directory at {series_dir}")
+
 # number of episodes is variable per series
 # it is always <30, so iterate through episodes up to 30
 
+successful_eps = []
 for number in range(1, 30):
     ep_url = series_url + f"episode-{str(number)}.srt"
     subs = requests.post(ep_url)
@@ -33,6 +46,6 @@ for number in range(1, 30):
         out_path = series_dir / f"series{args.series}_{str(number)}.txt"
         with open(out_path, "w") as f:
             f.write(subs.content.decode())
+        successful_eps.append(number)
 
-
-# save it in text format in the data dir
+logger.info(f"{str(len(successful_eps))} episodes found  - finished!")
